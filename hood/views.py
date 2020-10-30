56cf0f34e3a72807e1ce9867d092123a626c3fad
DerrickOdhiambo <odhiamboderrick56@gmail.com>
2020-10-30 19:28:53 +0300

    create the view for the homepage

--- hood/views.py
+++ hood/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render
+from django.views import generic
 
 
 def index(request):
+    return render(request, 'hood/index.html')
+
+
+def home(request):
     return render(request, 'hood/home.html')
