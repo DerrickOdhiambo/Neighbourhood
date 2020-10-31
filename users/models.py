8f57768b5f0d9c29faf12b8f69b4d17f49a62812
DerrickOdhiambo <odhiamboderrick56@gmail.com>
2020-10-31 22:59:53 +0300

    create the Profile model

--- users/models.py
+++ users/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django_resized import ResizedImageField
 
-# Create your models here.
+
+class Profile(models.Model):
+    user = models.OneToOneField(
+        User, on_delete=models.CASCADE)
+    id_number = models.IntegerField(default=0)
+    profile_picture = ResizedImageField(size=[300, 300], quality=75,
+                                        default='default.jpg', upload_to='profile_pics/')
+
+    def __str__(self):
+        return f'{self.user.username} UserProfile'
