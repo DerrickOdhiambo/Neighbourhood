8eee5132a30c957c1c8bc9b8277d355242cb8e9e
DerrickOdhiambo <odhiamboderrick56@gmail.com>
2020-10-31 22:49:05 +0300

    add aroute for the profile axxounts page

--- neighbourhood/urls.py
+++ neighbourhood/urls.py
@@ -15,10 +15,21 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from users import views as auth_views
+
+
+from django.conf import settings
+from django.conf.urls.static import static
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('hood.urls')),
     path('users/', include('django.contrib.auth.urls')),
     path('users/', include('users.urls')),
+    path('users/profile/', auth_views.profile, name='profile'),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL,
+                          document_root=settings.MEDIA_ROOT)
