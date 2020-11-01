c92e5b96c22a4819e8f4ffe205c2bdb40bd4f8e1
DerrickOdhiambo <odhiamboderrick56@gmail.com>
2020-11-01 10:52:24 +0300

    register the Tag model

--- hood/admin.py
+++ hood/admin.py
@@ -1,4 +1,8 @@
 from django.contrib import admin
-from .models import Post
+from .models import Post, Neighborhood, Occupant, Business, Tag
 
 admin.site.register(Post)
+admin.site.register(Neighborhood)
+admin.site.register(Occupant)
+admin.site.register(Business)
+admin.site.register(Tag)
