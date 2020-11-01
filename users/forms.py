afc2ee673dc27c9b4c1d883a3fa6f4a94208df49
DerrickOdhiambo <odhiamboderrick56@gmail.com>
2020-11-01 17:59:43 +0300

    add a neigbourhood field to the form

--- users/forms.py
+++ users/forms.py
@@ -31,4 +31,4 @@ class UserUpdateForm(forms.ModelForm):
 class ProfileUpdateForm(forms.ModelForm):
     class Meta:
         model = Profile
-        fields = ['profile_picture', 'id_number']
+        fields = ['profile_picture', 'id_number', 'neighborhood']
