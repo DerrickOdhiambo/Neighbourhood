5eb71c7151d3fa5cd7b726fb606c9da01198e35a
DerrickOdhiambo <odhiamboderrick56@gmail.com>
2020-11-01 17:58:58 +0300

    add neigbourhood column to the profile model

--- users/models.py
+++ users/models.py
@@ -1,12 +1,15 @@
 from django.db import models
 from django.contrib.auth.models import User
 from django_resized import ResizedImageField
+from hood.models import Neighborhood
 
 
 class Profile(models.Model):
     user = models.OneToOneField(
         User, on_delete=models.CASCADE)
     id_number = models.IntegerField(default=0)
+    neighborhood = models.ForeignKey(
+        Neighborhood, null=True, on_delete=models.CASCADE, blank=True)
     profile_picture = ResizedImageField(size=[300, 300], quality=75,
                                         default='default.jpg', upload_to='profile_pics/')
 
