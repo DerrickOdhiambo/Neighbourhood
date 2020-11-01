f3870e43ffa3181e74c7751f15233dfc19466be3
DerrickOdhiambo <odhiamboderrick56@gmail.com>
2020-11-01 18:01:50 +0300

    add url path for the neighborhoods

--- hood/urls.py
+++ hood/urls.py
@@ -8,4 +8,6 @@ urlpatterns = [
     path('home/', PostListView.as_view(), name='homepage'),
     path('post/<int:pk>/', PostDetailView.as_view(), name='post-detail'),
     path('post/new/', PostCreateView.as_view(), name='post-create'),
+    path('neighborhood/', views.display_hood, name='neighborhood'),
+    path('neighborhood/create/', views.create_hood, name='create_hood'),
 ]
