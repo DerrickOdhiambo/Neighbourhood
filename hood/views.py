95de019f13c773e90728a841f733f40146b836e2
DerrickOdhiambo <odhiamboderrick56@gmail.com>
2020-11-01 07:40:28 +0300

    create generic view for creating a new post

--- hood/views.py
+++ hood/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
-from django.views import generic
+from django.views.generic import ListView, DetailView, CreateView
+from .models import Post
 
 
 def index(request):
@@ -7,4 +8,27 @@ def index(request):
 
 
 def home(request):
-    return render(request, 'hood/home.html')
+    context = {
+        'posts': Post.objects.all()
+    }
+    return render(request, 'hood/home.html', context)
+
+
+class PostListView(ListView):
+    model = Post
+    template_name = 'hood/home.html'
+    context_object_name = 'posts'
+    ordering = ['-date_posted']
+
+
+class PostDetailView(DetailView):
+    model = Post
+
+
+class PostCreateView(CreateView):
+    model = Post
+    fields = ['title', 'content']
+
+    def form_valid(self, form):
+        form.instance.user = self.request.user
+        return super().form_valid(form)
