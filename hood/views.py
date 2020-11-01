afe6c933497192576876630d7efead84859e1da5
DerrickOdhiambo <odhiamboderrick56@gmail.com>
2020-11-01 18:03:40 +0300

    create a view for the neigbourhoods

--- hood/views.py
+++ hood/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views.generic import ListView, DetailView, CreateView
-from .models import Post
+from .models import Post, Neighborhood, Business
+from .forms import CreateNeighborhoodForm
 
 
 def index(request):
@@ -8,17 +9,22 @@ def index(request):
 
 
 def home(request):
-    context = {
-        'posts': Post.objects.all()
-    }
-    return render(request, 'hood/home.html', context)
+    return render(request, 'hood/home.html')
 
 
 class PostListView(ListView):
     model = Post
     template_name = 'hood/home.html'
-    context_object_name = 'posts'
     ordering = ['-date_posted']
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['posts'] = Post.objects.all()
+        context['businesses'] = Business.objects.all()
+        context['total_businesses'] = Business.objects.all().count()
+        context['total_neighborhoods'] = Neighborhood.objects.all().count()
+
+        return context
 
 
 class PostDetailView(DetailView):
@@ -32,3 +38,19 @@ class PostCreateView(CreateView):
     def form_valid(self, form):
         form.instance.user = self.request.user
         return super().form_valid(form)
+
+
+def display_hood(request):
+    neighborhoods = Neighborhood.objects.all()
+    return render(request, 'users/hood.html', {'hoods': neighborhoods})
+
+
+def create_hood(request):
+    form = CreateNeighborhoodForm()
+    if request == 'POST':
+        if form.is_valid():
+            form.save()
+        return redirect('users/hood.html')
+
+    context = {'form': form}
+    return render(request, 'users/create_hood.html', context)
