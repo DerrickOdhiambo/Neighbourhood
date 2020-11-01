11b81ce081cc63b0db401e0e579fa15cf936b4f3
DerrickOdhiambo <odhiamboderrick56@gmail.com>
2020-11-01 07:39:46 +0300

    add url for the post detail and creating a new post

--- hood/urls.py
+++ hood/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
 from . import views
+from .views import PostListView, PostDetailView, PostCreateView
 
 
 urlpatterns = [
     path('', views.index, name='landing-page'),
-    path('home/', views.home, name='homepage'),
+    path('home/', PostListView.as_view(), name='homepage'),
+    path('post/<int:pk>/', PostDetailView.as_view(), name='post-detail'),
+    path('post/new/', PostCreateView.as_view(), name='post-create'),
 ]
