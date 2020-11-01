9de83f63c80ea6437aa0d05c44a001393bc8eec9
DerrickOdhiambo <odhiamboderrick56@gmail.com>
2020-11-01 17:57:45 +0300

    unregister the Occupants model

--- hood/admin.py
+++ hood/admin.py
@@ -1,8 +1,7 @@
 from django.contrib import admin
-from .models import Post, Neighborhood, Occupant, Business, Tag
+from .models import Post, Neighborhood, Business, Tag
 
 admin.site.register(Post)
 admin.site.register(Neighborhood)
-admin.site.register(Occupant)
 admin.site.register(Business)
 admin.site.register(Tag)
