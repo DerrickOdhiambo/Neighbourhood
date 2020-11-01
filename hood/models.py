e182c719d6375c217b73dd45858fc8482bd292a6
DerrickOdhiambo <odhiamboderrick56@gmail.com>
2020-11-01 07:38:20 +0300

    import timezone to capture the date and time post was made

--- hood/models.py
+++ hood/models.py
@@ -1,11 +1,13 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.utils import timezone
+from django.urls import reverse
 
 
 class Post(models.Model):
     title = models.CharField(max_length=100)
     content = models.TextField()
-    date_posted = models.DateTimeField(auto_now_add=True)
+    date_posted = models.DateTimeField(default=timezone.now)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
 
     def __str__(self):
@@ -13,3 +15,6 @@ class Post(models.Model):
 
     def save_post(self):
         self.save()
+
+    def get_absolute_url(self):
+        return reverse('post-detail', kwargs={'pk': self.pk})
