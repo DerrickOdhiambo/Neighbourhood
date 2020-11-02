47f36fbc365a3c355ea422181e8b44d610aee826
DerrickOdhiambo <odhiamboderrick56@gmail.com>
2020-11-02 06:01:14 +0300

    add a search url path

--- hood/urls.py
+++ hood/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from . import views
-from .views import PostListView, PostDetailView, PostCreateView
+from .views import PostListView, PostDetailView, PostCreateView, SearchListView
 
 
 urlpatterns = [
@@ -9,5 +9,10 @@ urlpatterns = [
     path('post/<int:pk>/', PostDetailView.as_view(), name='post-detail'),
     path('post/new/', PostCreateView.as_view(), name='post-create'),
     path('neighborhood/', views.display_hood, name='neighborhood'),
-    path('neighborhood/create/', views.create_hood, name='create_hood'),
+    path('search/', views.search, name='search'),
+    path('search/search_business/',
+         SearchListView.as_view(), name='search-business'),
+    path('create/', views.create_hood, name='create_hood'),
+    path('update/<int:pk>', views.update_hood, name='update_hood'),
+    path('delete/<int:pk>', views.delete_hood, name='delete_hood'),
 ]
