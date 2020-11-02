61a26d94f83498bbffac87fd28ba618696a13ca2
DerrickOdhiambo <odhiamboderrick56@gmail.com>
2020-11-02 13:22:08 +0300

    remove labels from the register form

--- users/forms.py
+++ users/forms.py
@@ -5,12 +5,18 @@ from .models import Profile
 
 
 class RegisterForm(UserCreationForm):
-    first_name = forms.CharField(max_length=20, widget=forms.TextInput(
-        attrs={'class': 'form-control'}))
-    last_name = forms.CharField(max_length=20, widget=forms.TextInput(
-        attrs={'class': 'form-control'}))
-    email = forms.EmailField(widget=forms.EmailInput(
-        attrs={'class': 'form-control'}))
+    username = forms.CharField(label=False, max_length=20, widget=forms.TextInput(
+        attrs={'class': 'form-control', 'placeholder': 'Username...'}))
+    first_name = forms.CharField(label=False, max_length=20, widget=forms.TextInput(
+        attrs={'class': 'form-control', 'placeholder': 'First Name...'}))
+    last_name = forms.CharField(label=False, max_length=20, widget=forms.TextInput(
+        attrs={'class': 'form-control', 'placeholder': 'Last Name...'}))
+    email = forms.EmailField(label=False, widget=forms.EmailInput(
+        attrs={'class': 'form-control', 'placeholder': 'Email..'}))
+    password1 = forms.CharField(label=False, widget=forms.PasswordInput(
+        attrs={"class": "form-control", "placeholder": "Password..."}))
+    password2 = forms.CharField(label=False, widget=forms.PasswordInput(
+        attrs={"class": "form-control", "placeholder": "Confirm Password..."}))
 
     class Meta:
         model = User
